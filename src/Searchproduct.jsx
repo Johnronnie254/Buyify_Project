c175cc54307b495d0ae04c727043752b59690efc
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-11-11 10:07:56 +0300

    You can successfully search for a product in the searchbar

--- src/Searchproduct.jsx
+++ src/Searchproduct.jsx
@@ -3,7 +3,16 @@ import Products from "./products";
 
 function Searchproduct() {
   const [products, setProducts] = useState([]);
-  const [visible, setVisible] = useState(2);
+  const [visible, setVisible] = useState(4);
+  const [search, setSearch] = useState("");
+  const LoadMoreData = () => {
+    setVisible((prevVisible) => prevVisible + 4);
+  };
+
+  const LoadLessData = () => {
+    setVisible((prevVisible) => prevVisible - 4);
+  };
+
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
@@ -11,16 +20,26 @@ function Searchproduct() {
   }, []);
   return (
     <>
-      <input type="text" placeholder="enter your product" />
+      <input
+        className="input"
+        type="text"
+        placeholder="enter your product"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <div>
-        {products.map.slice(
-          0,
-          visible
-        )((product) => (
-          <Products key={product.id} {...product} />
-        ))}
+        {products
+          .filter((product) => {
+            return product.title.toLowerCase() === ""
+              ? product
+              : product.title.toLowerCase().includes(search);
+          })
+          .slice(0, visible)
+          .map((product) => (
+            <Products key={product.id} {...product} />
+          ))}
       </div>
-      <button>Load More</button>
+      <button onClick={LoadMoreData}>Load More</button>
+      <button onClick={LoadLessData}>Load Less</button>
     </>
   );
 }
