a41b293b3bc9a51cf874edb616f552de8da38bcd
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-11-12 07:40:04 +0300

    cards need background color

--- src/products.jsx
+++ src/products.jsx
@@ -1,22 +1,9 @@
-// Products.jsx
+
 import React from "react";
 import { useCart } from "react-use-cart";
 
-function getRandomColor() {
-  // Function to generate a random color
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+//need to add cardColor generator
 
-function Products(product) {
-  const { addItem } = useCart();
-
-  // Generate a random background color for each card
-  const cardColor = getRandomColor();
 
   return (
     <div
